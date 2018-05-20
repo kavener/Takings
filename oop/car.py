89c7a82296d9b4331742e9418eacd8eced6e81c3
liang <271097513@qq.com>
2018-05-20 22:49:35 +0800

    lll

--- oop/car.py
+++ oop/car.py
@@ -1,21 +1,17 @@
 import re
 
-
 class Car():
     def __init__(self, make, model, year):
         '''初始化描述汽车的属性'''
         self.make = make
         self.model = model
         self.year = year
-
+        '''添加一个初始是为0的类'''
     def get_describe_name(self):
         '''返回整洁的描述性信息.'''
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
 
         return long_name
-
-
-
 
 print("---------")
 my_new_car = Car('audi', 'a4', '2018')
