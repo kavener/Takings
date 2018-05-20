1772012f4b90a750a563cd868011905d61c3098f
liang <271097513@qq.com>
2018-05-20 20:54:18 +0800

    lll

--- oop/car.py
+++ oop/car.py
@@ -1,18 +1,27 @@
+import re
+
+
 class Car():
-    def __init__(self,make,model,year):
+    def __init__(self, make, model, year):
         '''初始化描述汽车的属性'''
-        self.make=make
-        self.model=model
-        self.year=year
+        self.make = make
+        self.model = model
+        self.year = year
 
     def get_describe_name(self):
         '''返回整洁的描述性信息.'''
-        long_name=str(self.year) + ' ' + self.make + ' ' + self.model
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+
         return long_name
+
+
+
+
 print("---------")
-my_new_car=Car('audi','a4','2018')
+my_new_car = Car('audi', 'a4', '2018')
 print(my_new_car.get_describe_name())
 print("改变没有呢")
 print("测试集体改变")
 print("测试集体改变")
 print("测试，传送")
+print("转换不同项目，然后切换到回来，是否托管")
