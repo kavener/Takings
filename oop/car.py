1dc39a82390bbdfbdd25010def222f9a85d0549d
liang <271097513@qq.com>
2018-05-21 19:30:21 +0800

    Ruo

--- oop/car.py
+++ oop/car.py
@@ -29,10 +29,10 @@ class Car():
             self.odometer_reading = mileage
         else:
             print("FIB Forbidden!!!")
-    def incremnt_odometer(self,miles):
+
+    def incremnt_odometer(self, miles):
         # 将里程表读数增加指定的量
         self.odometer_reading += miles
-
 
 
 my_new_car = Car('audi', 'a4', '2018')
@@ -56,6 +56,18 @@ my_new_car.read_odometer()
 my_new_car.incremnt_odometer(10)
 my_new_car.read_odometer()
 
+
+# 将实例用作属性
+class Battery():
+
+    # 模拟电动汽车的简单尝试
+    def __init__(self, battery=70):
+        self.battery = battery
+
+    def describe_battery(self):
+        # 打印一条描述电瓶容量的信息
+        print("The car has a " + str(self.battery) + "-kwh battery.")
+
 # 关于继承
 # 子类继承父类的所有属性和方法，同时还可以定义自己的属性和方法。
 class ElectricCar(Car):
@@ -64,6 +76,20 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         # 初始化父类的属性
         super().__init__(make, model, year)
+        # 给子类定义自己的属性
+        self.battery_size = 70
+        self.battery = Battery
+    def describe_battery(self):
+        print("The car has a " + str(self.battery_size) + "-kwh battery.")
+
+    # 重写父类的方法及重新定义新的同名方法，在调用同名方法时，\
+    # Python会忽略父类方法直接调用子类重新定义的方法
+
 
 my_tesla = ElectricCar("tesla", "model s", 2018)
 print(my_tesla.get_describe_name())
+my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+
+
+
