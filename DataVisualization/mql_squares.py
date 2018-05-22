a9c385eb2f28d4fe8a7395e9c4edd56c075e131d
liang <271097513@qq.com>
2018-05-22 16:05:12 +0800

    Ruo

--- DataVisualization/mql_squares.py
+++ DataVisualization/mql_squares.py
@@ -1,6 +1,14 @@
 from matplotlib import pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
+input_valuse = [1, 2, 3, 4, 5]
+# 设置样式
+plt.plot(input_valuse, squares, linewidth=5)
 
-plt.plot(squares)
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Values", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+plt.tick_params(axis="both", labelsize=14)
+
 plt.show()
